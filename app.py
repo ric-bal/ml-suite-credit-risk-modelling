2f7f6fa36dcb4ea6acf32975b33334178fb13fad
ric-bal <4richbal@gmail.com>
2026-09-13 21:06:18 +0100

    populate tabs

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 import joblib
+import io
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+
+#sns.set_style('darkgrid')
+# plt.rcParams.update({
+#     'savefig.facecolor': '#0E1117',
+#     'figure.facecolor': '#0E1117',
+#     'axes.facecolor': '#161B22',
+#     'axes.edgecolor': '#30363D',
+#     'axes.labelcolor': '#E6EDF3',
+#     'text.color': '#E6EDF3',
+#     'xtick.color': '#8B949E',
+#     'ytick.color': '#8B949E',
+#     'grid.color': '#30363D',
+#     'grid.alpha': 0.5,
+#     'font.size': 10
+# })
 
 st.set_page_config(layout='wide')
-cols = st.columns([2,3], border=True, gap='medium')#, width=100000000)#, gap='xlarge')
+cols = st.columns([2,3], border=True, gap='medium')
 
 if 'pred' not in st.session_state:
     st.session_state.pred = None
@@ -20,8 +39,6 @@ with cols[0]:
     model_dict = {}
     for p in model_paths:
         model_dict[p.name.replace('.pkl', '').replace('_', ' ')] = p
-
-    
 
     col1, col2= st.columns([3,2])
     with col1:
@@ -57,10 +74,11 @@ with cols[0]:
 
 
 with cols[1]:
-    tab1, tab2 = st.tabs([
+    tab1, tab2, tab3 = st.tabs([
         'Results',
-        'Data'
-    ])
+        'Data Overview',
+        'EDA'
+    ], height='stretch', width='stretch')
 
     with tab1:
         if st.session_state.pred:
@@ -69,7 +87,90 @@ with cols[1]:
             st.error(f'Predicted Credit Risk: BAD')
 
     with tab2:
-        st.header('qwe34')
+        data = {name: joblib.load(f'data/{name}.pkl') for name in ['df_model', 'X_train', 'X_test', 'y_train', 'y_test']}
+        df = data['df_model']
+
+        st.header('Data Summary')
+        st.text(f'Dataframe shape: {df.shape}')
+        st.dataframe(df)
+        
+    
+        st.subheader('Data Info Output')
+        buffer = io.StringIO()
+        df.info(buf=buffer, memory_usage=True, show_counts=False)
+        st.code(buffer.getvalue())
+
+        st.subheader('Data Describe Output')
+        st.dataframe(df.describe(include='all').T)
+
+    with tab3:
+        st.header('Exploratory Data Analysis')
+
+        # distribution of numerical features - bar charts
+        st.subheader('Distribution of Numerical Features')
+        cols = ['age', 'credit_amount', 'duration']
+        col1, col2, col3 = st.columns(3, gap='xsmall')
+
+        for container, feature in zip([col1, col2, col3], cols):
+            fig = px.histogram(
+                df,
+                x=feature,
+                nbins=10,
+            )
+
+            fig.update_layout(
+                title=dict(
+                    text=feature,
+                    x=0.5,
+                    xanchor='center',
+                    y=0.98,
+                    yanchor='top'
+                ),
+                height=350,
+                margin=dict(l=10, r=10, t=50, b=10),
+
+                paper_bgcolor="#14191F",
+                plot_bgcolor='#161B22',
+            )
+
+            fig.update_traces(
+                marker_line_color="#141415",
+                marker_line_width=2
+            )
+
+            container.plotly_chart(
+                fig,
+                width='stretch'
+            )
+
+        # distribution of numerical features - boxplots
+        cols = ['age', 'credit_amount', 'duration']
+        col1, col2, col3 = st.columns(3, gap='xsmall')
+
+        for container, feature in zip([col1, col2, col3], cols):
+            fig = px.box(
+                df,
+                y=feature,
+            )
+
+            fig.update_layout(
+                height=350,
+                margin=dict(l=10, r=10, t=50, b=10),
+
+                paper_bgcolor="#14191F",
+                plot_bgcolor='#161B22',
+            )
+
+            fig.update_traces(
+                marker_line_color="#141415",
+                marker_line_width=2
+            )
+
+            container.plotly_chart(
+                fig,
+                width='stretch'
+            )
+        st.write('All right skewed, mode < median < mean. ```credit_amount``` is especially right skewed - many datapoints above Q3.')
 
 
 
